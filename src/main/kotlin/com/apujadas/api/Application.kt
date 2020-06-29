a30481529e6dcf11a8c3836008d65be3392367a1
Andres Blas Pujadas <andres.pujadas@mercadolibre.com>
2020-06-28 23:18:30 -0300

    Application and application.yml adding :wrench:

---
+++ src/main/kotlin/com/apujadas/api/Application.kt
@@ -0,0 +1,11 @@
+package com.apujadas.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+open class Application
+
+fun main(args: Array<String>) {
+    runApplication<Application>(*args)
+}
